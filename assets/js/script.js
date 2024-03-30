54538352f0200c741f5ec9830faedbcad27aedc6
BHUSHAN9535 <balma9242@gmail.com>
2024-03-30 16:46:30 +0530

    chnage

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,93 +1,101 @@
-window.onload = function() {
-    openAlert();
-  };
-  
-  function openAlert() {
-    var customAlert = document.getElementById('customAlert');
-    var message = "Please fill in the following details:";
-    document.getElementById('alertMessage').textContent = message;
-    customAlert.style.display = 'block';
-    
-    document.getElementById('userInfoForm').addEventListener('submit', function(event) {
-      event.preventDefault();
-      var formData = new FormData(this);
-      var userData = {};
-      for (var pair of formData.entries()) {
-        userData[pair[0]] = pair[1];
-      }
-      console.log(userData);
-      showSuccessNotification();
-      closeAlert();
-    });
-  }
-  
-  function closeAlert() {
-    var customAlert = document.getElementById('customAlert');
-    customAlert.style.display = 'none';
-  }
-  
-  function showSuccessNotification() {
-    var notification = document.createElement('div');
-    notification.className = 'notification';
-    var img = document.createElement('img');
-    img.src = 'https://github.com/BHUSHAN9534/KOSHI-SHIKSHA-PVT-LIMITED/blob/main/assets/images/success.gif?raw=true'; // Provide the path to your success image
-    notification.appendChild(img);
-    var message = document.createElement('p');
-    message.textContent = 'You have successfully submitted!';
-    notification.appendChild(message);
-    document.body.appendChild(notification);
-    
-    setTimeout(function() {
-      notification.style.opacity = '0';
-      setTimeout(function() {
-        document.body.removeChild(notification);
-      }, 1000); // Adjust the delay before removing the notification
-    }, 3000); // Adjust the duration for which the notification is displayed
-  }
-  
+window.onload = function () {
+  openAlert();
+};
+
+function openAlert() {
+  var customAlert = document.getElementById('customAlert');
+  var message = "Please fill in the following details:";
+  document.getElementById('alertMessage').textContent = message;
+  customAlert.style.display = 'block';
+
+  document.getElementById('userInfoForm').addEventListener('submit', function (event) {
+    event.preventDefault();
+    var formData = new FormData(this);
+    var userData = {};
+    for (var pair of formData.entries()) {
+      userData[pair[0]] = pair[1];
+    }
+    var name = document.getElementById('name').value.trim();
+    var mobile = document.getElementById('mobile').value.trim();
+    var course = document.getElementById('course').value;
+
+    if (!name || !mobile || !course) {
+      alert("Name, Mobile, and Course are mandatory fields!");
+      return;
+    }
+    console.log(userData);
+    showSuccessNotification();
+    closeAlert();
+  });
+}
+
+function closeAlert() {
+  var customAlert = document.getElementById('customAlert');
+  customAlert.style.display = 'none';
+}
+
+function showSuccessNotification() {
+  var notification = document.createElement('div');
+  notification.className = 'notification';
+  var img = document.createElement('img');
+  img.src = 'https://github.com/BHUSHAN9534/KOSHI-SHIKSHA-PVT-LIMITED/blob/main/assets/images/success.gif?raw=true'; // Provide the path to your success image
+  notification.appendChild(img);
+  var message = document.createElement('p');
+  message.textContent = 'You have successfully submitted!';
+  notification.appendChild(message);
+  document.body.appendChild(notification);
+
+  setTimeout(function () {
+    notification.style.opacity = '0';
+    setTimeout(function () {
+      document.body.removeChild(notification);
+    }, 1000); // Adjust the delay before removing the notification
+  }, 3000); // Adjust the duration for which the notification is displayed
+}
+
 
 
 //   Show More Section
 
 function toggleCourses() {
-    var moreCourses = document.querySelectorAll('.show-more-section .course');
-    var showMoreSection = document.querySelector('.show-more-section');
-    var buttonText = document.querySelector('.show-more-button button');
+  var moreCourses = document.querySelectorAll('.show-more-section .course');
+  var showMoreSection = document.querySelector('.show-more-section');
+  var buttonText = document.querySelector('.show-more-button button');
 
-    if (showMoreSection.style.display === 'none') {
-        showMoreSection.style.display = 'block';
-        buttonText.innerText = 'Show Less';
-    } else {
-        showMoreSection.style.display = 'none';
-        buttonText.innerText = 'Show More';
-    }
+  if (showMoreSection.style.display === 'none') {
+    showMoreSection.style.display = 'block';
+    buttonText.innerText = 'Show Less';
+  } else {
+    showMoreSection.style.display = 'none';
+    buttonText.innerText = 'Show More';
+  }
 }
 
 $('.testimonial_owlCarousel').owlCarousel({
-loop:true,
-margin:10,
-dots:false,
-nav:true,
-autoplay:false,   
-smartSpeed: 3000, 
-autoplayTimeout:4000,
-responsive:{
-0:{
-items:1
-},
-600:{
-items:1
-},
-1000:{
-items:1
-}
-}
+  loop: true,
+  margin: 10,
+  dots: false,
+  nav: true,
+  autoplay: false,
+  smartSpeed: 3000,
+  autoplayTimeout: 4000,
+  responsive: {
+    0: {
+      items: 1
+    },
+    600: {
+      items: 1
+    },
+    1000: {
+      items: 1
+    }
+  }
 })
 
 
 //Start Our Branches Offices Location
 
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", function () {
   // Get references to the dropdowns and details box
   var categoryDropdown = document.getElementById('categoryDropdown');
   var itemDropdown = document.getElementById('itemDropdown');
@@ -99,7 +107,7 @@ document.addEventListener("DOMContentLoaded", function() {
   // Define the options for each category along with their details
   var options = {
     AndhraPradesh: {
-        Anantapur: {
+      Anantapur: {
         description: "Apples are nutritious Andhra Pradesh.",
         image: "https://example.com/apple-image.jpg",
         link: "https://en.wikipedia.org/wiki/Apple",
@@ -232,7 +240,7 @@ document.addEventListener("DOMContentLoaded", function() {
 
     },
     ArunachalPradesh: {
-        Tawang: {
+      Tawang: {
         description: "Carrots are rich in vitamin A.",
         image: "https://example.com/carrot-image.jpg",
         link: "https://en.wikipedia.org/wiki/Carrot",
@@ -324,7 +332,7 @@ document.addEventListener("DOMContentLoaded", function() {
       },
     },
     Assam: {
-        Kamrup: {
+      Kamrup: {
         description: "Carrots are rich in vitamin A.",
         image: "https://example.com/carrot-image.jpg",
         link: "https://en.wikipedia.org/wiki/Carrot",
@@ -416,7 +424,7 @@ document.addEventListener("DOMContentLoaded", function() {
       },
     },
     Bihar: {
-        Araria_अररिया: {
+      Araria_अररिया: {
         description: "Carrots are rich in vitamin A.",
         image: "https://example.com/carrot-image.jpg",
         link: "https://en.wikipedia.org/wiki/Carrot",
@@ -808,7 +816,7 @@ document.addEventListener("DOMContentLoaded", function() {
       },
     },
     Chhattisgarh: {
-        Raipur_रायपुर: {
+      Raipur_रायपुर: {
         description: "Carrots are rich in vitamin A.",
         image: "https://example.com/carrot-image.jpg",
         link: "https://en.wikipedia.org/wiki/Carrot",
@@ -1327,7 +1335,7 @@ document.addEventListener("DOMContentLoaded", function() {
     itemDropdown.innerHTML = '<option value="">Select Item</option>';
     // Add options based on the selected category
     if (selectedCategory && options[selectedCategory]) {
-      Object.keys(options[selectedCategory]).forEach(function(item) {
+      Object.keys(options[selectedCategory]).forEach(function (item) {
         var option = document.createElement('option');
         option.textContent = item;
         option.value = item;
@@ -1356,9 +1364,9 @@ document.addEventListener("DOMContentLoaded", function() {
       // Add additional details
       var detailsList = document.createElement('ul');
       detailsList.innerHTML = '<li><strong>Name:</strong> ' + selectedItemDetails.name + '</li>' +
-                              '<li><strong>Contact:</strong> ' + selectedItemDetails.contact + '</li>' +
-                              '<li><strong>Email:</strong> <a href="mailto:' + selectedItemDetails.email + '">' + selectedItemDetails.email + '</a></li>' +
-                              '<li><strong>Address:</strong> ' + selectedItemDetails.address + '</li>';
+        '<li><strong>Contact:</strong> ' + selectedItemDetails.contact + '</li>' +
+        '<li><strong>Email:</strong> <a href="mailto:' + selectedItemDetails.email + '">' + selectedItemDetails.email + '</a></li>' +
+        '<li><strong>Address:</strong> ' + selectedItemDetails.address + '</li>';
       detailsParagraph.appendChild(detailsList);
       // Add Google Maps location
       var mapElement = document.createElement('iframe');
